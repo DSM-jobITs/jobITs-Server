b2bb292d1114db58870d3243a2551026fd7bca7d
MinJunSeo <smj0385@naver.com>
2020-09-30 16:46:35 +0900

    Add get question function without field value

--- src/routes/interviewRouter.js
+++ src/routes/interviewRouter.js
@@ -6,14 +6,18 @@ const interview = new Interview(Interviews);
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  const { page, keyword } = req.query;
+router.get('/', async (req, res) => {
+  const { page } = req.query;
+  // field 값이 없다면 모든 field를 가져올 수 있게 하기 위해 false를 대입
+  // mysql의 모든 문자열은 boolean으로 바꾸면 false이다.
+  const field = req.query.field ? req.query.field : false;
   const maxShow = 10;  // 현재 10개로 고정해두기로 결정
-  
-  if (keyword) {
-  } else {
-    const results = await interview.getAllFieldQuestions(page, maxShow);
+
+  try {
+    const results = await interview.getInterviewQuestions(page, field, maxShow);
     res.status(200).send(results);
+  } catch (error) {
+    console.error(error);
   }
 
 });
