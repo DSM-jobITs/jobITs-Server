15b785877f2b344822c4b4fdddffeae137554289
MinJunSeo <smj0385@naver.com>
2020-09-30 11:22:34 +0900

    Connect with database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const { connectDatabase } = require('./models/connection');
 const app = express();
 const { SERVER_PORT } = require('./config');
+
+connectDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(SERVER_PORT, () => {
-  console.log(`SERVER is starting at ${SERVER_PORT} port.`);
+  console.log(`Server is starting at ${SERVER_PORT} port.`);
 });
