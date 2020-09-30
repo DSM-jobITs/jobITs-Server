7c982786e32af2303e46d6d18bbb6589bae38457
MinJunSeo <smj0385@naver.com>
2020-09-30 15:47:46 +0900

    Add interview router

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { connectDatabase } = require('./models/connection');
 const app = express();
 const { SERVER_PORT } = require('./config');
+const interviewRouter = require('./routes/interviewRouter');
 
 connectDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/interview', interviewRouter);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is starting at ${SERVER_PORT} port.`);
