78b77170807a9cbfc1342928267d119a098ca0d3
MinJunSeo <smj0385@naver.com>
2020-09-30 11:08:03 +0900

    Add init express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('test');
+const express = require('express');
+const app = express();
+const { SERVER_PORT } = require('./config');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(SERVER_PORT, () => {
+  console.log(`SERVER is starting at ${SERVER_PORT} port.`);
+});
