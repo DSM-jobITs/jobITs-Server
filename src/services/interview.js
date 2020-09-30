43193af6a2212d16a735d8c8e2c6e46d7e9547e4
MinJunSeo <smj0385@naver.com>
2020-09-30 17:05:20 +0900

    Fix check result is empty

--- src/services/interview.js
+++ src/services/interview.js
@@ -1,13 +1,24 @@
+const { badRequest, notFound } = require('../errors');
+
 class InterviewService {
   constructor(interviewModel) {
     this.interviewModel = interviewModel;
   }
 
-  async getAllFieldQuestions(page, maxShow) {
+  async getInterviewQuestions(page, field, maxShow) {
+    if (!page) {
+      throw badRequest;
+    }
+    
     const results = await this.interviewModel.findAll({
+      where: { field: field },
       limit: maxShow,
       offset: page - 1
     });
+
+    if (!results.length) {
+      throw notFound;
+    }
 
     results.forEach((result) => {
       result['createdAt'] = result['createdAt'].toFormat('YYYY-MM-DD');
