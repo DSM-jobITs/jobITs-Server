84354b7a0e707ae10ce78d16a6d0fbcad8732761
MinJunSeo <smj0385@naver.com>
2020-10-01 11:50:30 +0900

    Fix field to get serval fields

--- src/routes/interviewRouter.js
+++ src/routes/interviewRouter.js
@@ -10,12 +10,12 @@ router.get('/', async (req, res) => {
   const { page } = req.query;
   // field 값이 없다면 모든 field를 가져올 수 있게 하기 위해 false를 대입
   // mysql의 모든 문자열은 boolean으로 바꾸면 false이다.
-  const field = req.query.field ? req.query.field : false;
+  const field = req.query.field ? req.query.field.split(',') : false;
   const maxShow = 10;  // 현재 10개로 고정해두기로 결정
 
   try {
     const results = await interview.getInterviewQuestions(page, field, maxShow);
-    res.status(200).send(results);
+    res.send(results);
   } catch (error) {
     res.status(error.status).send(error.message);
   }
