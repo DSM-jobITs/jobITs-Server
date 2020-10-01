9d83832bb433f6398b8e270e8b4167bbb12271c0
MinJunSeo <smj0385@naver.com>
2020-10-01 12:10:37 +0900

    Fix split query if alone field is okay

--- src/routes/interviewRouter.js
+++ src/routes/interviewRouter.js
@@ -10,12 +10,22 @@ router.get('/', async (req, res) => {
   const { page } = req.query;
   // field 값이 없다면 모든 field를 가져올 수 있게 하기 위해 false를 대입
   // mysql의 모든 문자열은 boolean으로 바꾸면 false이다.
-  const field = req.query.field ? req.query.field.split(',') : false;
+  const field = req.query.field ? (req.query.field).split(',') : false;
   const maxShow = 10;  // 현재 10개로 고정해두기로 결정
 
   try {
     const results = await interview.getInterviewQuestions(page, field, maxShow);
-    res.send(results);
+    res.send({ lists: results });
+  } catch (error) {
+    res.status(error.status).send(error.message);
+  }
+});
+
+router.post('/', async (req, res) => {
+  const { questions, field } = req.body;
+
+  try {
+    res.status(201).send({ message: '성공적으로 면접 질문을 등록하였습니다.' });
   } catch (error) {
     res.status(error.status).send(error.message);
   }
