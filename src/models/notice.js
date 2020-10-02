c2fbc7b2c71686092f04aa093acd9cea03e8f97f
MinJunSeo <smj0385@naver.com>
2020-10-02 14:33:21 +0900

    Add notices database model

---
+++ src/models/notice.js
@@ -0,0 +1,38 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('./connection');
+
+class Notices extends Model {}
+
+Notices.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING(40),
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING(2000),
+    allowNull: false
+  },
+  file: {
+    type: DataTypes.STRING(400)
+  },
+  fixed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  createdAt: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  sequelize,
+  tableName: 'notices'
+});
+
+module.exports = Notices;
