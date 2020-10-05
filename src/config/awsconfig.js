e7a01e0165bfa473d3a90fbd488c63a59323ef0f
MinJunSeo <smj0385@naver.com>
2020-10-05 21:36:11 +0900

    Add bucket name info

--- src/config/awsconfig.js
+++ src/config/awsconfig.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
   SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
-  REGION: 'ap-northeast-2'
+  REGION: 'ap-northeast-2',
+  BUCKET_NAME: process.env.BUCKET_NAME
 };
