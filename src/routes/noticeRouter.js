42651235972181ce6144e0d9bdfe50e48604112d
MinJunSeo <smj0385@naver.com>
2020-10-05 22:36:35 +0900

    Add aws setting

--- src/routes/noticeRouter.js
+++ src/routes/noticeRouter.js
@@ -1,31 +1,46 @@
 const express = require('express');
+const router = express.Router();
 
 const Notices = require('../models/notice');
 const NoticeService = require('../services/notice');
 const noticeService = new NoticeService(Notices);
 
+const path = require('path');
+const fs = require('fs');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+AWS.config.loadFromPath(__dirname + '/../config/awsconfig.json');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
-      cb(null, 'uploads/images');
-    } else if (file.mimetype === 'application/pdf' || file.mimetype === 'text/plain') {
-      cb(null, 'uploads/texts');
-    } else {
-      console.log(file.mimetype);
-      cb({ error: 'Mime type not supported!' });
-    }
-  }
-});
+const { BUCKET_NAME } = require('../config');
+const s3 = new AWS.S3();
 
-const upload = multer({ storage: storage });
-
-const router = express.Router();
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: BUCKET_NAME,
+    key: (req, file, cb) => {
+      const extension = path.extname(file.originalname);
+      cb(null, Date.now().toString() + extension);
+    },
+    acl: 'public-read'
+  })
+})
 
 router.post('/', upload.array('test'), async (req, res) => {
   req.files.forEach((file) => {
-    console.log(file);
+    const param = {
+      Bucket: BUCKET_NAME,
+      Key: file.filename,
+      ACL: 'public-read',
+      Body: fs.readFileSync(file.filename)
+    };
+    s3.upload(param, (error, data) => {
+      if (error) {
+        throw error;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+    });
   });
   res.send('Success');
 });
