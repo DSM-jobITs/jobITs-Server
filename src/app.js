a02edbd8265185d277739ba479fcf72207ce0aee
MinJunSeo <smj0385@naver.com>
2020-10-05 13:17:54 +0900

    Fix error undefined port value

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { connectDatabase } = require('./models/connection');
 
 const app = express();
-const { PORT } = require('./config');
+const { SERVER_PORT } = require('./config');
+const noticeRouter = require('./routes/noticeRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 connectDatabase();
 
-app.listen(PORT, () => {
-  console.log(`Start the server at ${PORT}`);
+app.use('/notice', noticeRouter);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Start the server at ${SERVER_PORT}`);
 });
