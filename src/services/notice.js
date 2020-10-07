fab6efa0db3d6b47f4a413bdb19cde66baa3b0ec
MinJunSeo <smj0385@naver.com>
2020-10-07 17:07:37 +0900

    Use FileService

--- src/services/notice.js
+++ src/services/notice.js
@@ -1,9 +1,11 @@
-class NoticeService {
+const FileService = require('./file');
+
+class NoticeService extends FileService {
   constructor(noticeModel) {
     this.noticeModel = noticeModel;
   }
 
-
+  
 }
 
 module.exports = NoticeService;
