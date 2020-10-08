27c169ad63c15b1cfbbca42d38f3623c4ac2b024
MinJunSeo <smj0385@naver.com>
2020-10-08 14:44:04 +0900

    Add create notice post service

--- src/services/notice.js
+++ src/services/notice.js
@@ -1,11 +1,25 @@
 const FileService = require('./file');
+const { badRequest } = require('../errors');
 
 class NoticeService extends FileService {
   constructor(noticeModel) {
     this.noticeModel = noticeModel;
   }
 
-  
+  async createNotice(title, content, files, fixed) {
+    if (!title || !content || !fixed) {
+      throw badRequest;
+    }
+    
+    files.forEach((file) => {
+      await this.noticeModel.create({
+        title: title,
+        content: content,
+        file: file,
+        fixed: fixed
+      });
+    });
+  }
 }
 
 module.exports = NoticeService;
