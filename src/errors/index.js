deff9e4cf8e62d7520a6a972f1782fcb86c17096
MinJunSeo <smj0385@naver.com>
2020-10-09 19:57:42 +0900

    Add 404 Not Found error

--- src/errors/index.js
+++ src/errors/index.js
@@ -6,7 +6,9 @@ class HttpError extends Error {
 }
 
 const badRequest = new HttpError(400, '올바르지 않은 요청입니다.');
+const notFound = new HttpError(404, '게시물이 존재하지 않습니다.');
 
 module.exports = {
-  badRequest
+  badRequest,
+  notFound
 };
