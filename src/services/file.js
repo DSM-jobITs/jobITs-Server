2cfb9127f16f2e1317c72ba8e90f2e9e13123509
MinJunSeo <smj0385@naver.com>
2020-10-09 10:00:21 +0900

    Use uuid function to make file key

--- src/services/file.js
+++ src/services/file.js
@@ -1,6 +1,8 @@
 const path = require('path');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const makeUuid = require('../utils/makeUuid');
+const { badRequest } = require('../errors');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
 
 const AWS = require('aws-sdk');
 AWS.config.loadFromPath(__dirname + '/../config/awsconfig.json');
@@ -8,48 +10,56 @@ AWS.config.loadFromPath(__dirname + '/../config/awsconfig.json');
 const { BUCKET_NAME } = require('../config');
 const s3 = new AWS.S3();
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: BUCKET_NAME,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: (req, file, cb) => {
-      const extension = path.extname(file.originalname);
-      cb(null, Date.now().toString() + extension);
-    },
-    acl: 'public-read'
-  })
-});
+// const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: BUCKET_NAME,
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     key: (req, file, cb) => {
+//       const extension = path.extname(file.originalname);
+//       cb(null, Date.now().toString() + extension);
+//     },
+//     acl: 'public-read'
+//   })
+// });
 
 class FileService {
-  constructor() {
-    this.upload = upload;
+  makeFileKey(file) {
+    if (!file) {
+      throw badRequest;
+    }
+
+    const extension = path.extname(file.name);
+    const uuid = makeUuid();
+    return uuid + extension;
   }
 
   async uploadFiles(files) {
-    if (!files) {
+    if (!files.length) {
       return [null];
     }
 
     let fileLocationArray = [];
     
-    await files.forEach((file) => {
-      const params = {
-        Bucket: BUCKET_NAME,
-        Key: file.key,
-        Body: file.location,
-        ACL: 'public-read'
-      };
+    
 
-      s3.upload(params, (error, data) => {
-        if (error) {
-          error.status = 500;
-          throw error;
-        }
-        fileLocationArray.push(data.Location);
-      });
-    });
-    return fileLocationArray;
+    // files.forEach((file) => {
+    //   const params = {
+    //     Bucket: BUCKET_NAME,
+    //     Key: this.makeFileKey(file),
+    //     Body: file.path,
+    //     ACL: 'public-read'
+    //   };
+
+    //   s3.upload(params, (error, data) => {
+    //     if (error) {
+    //       error.status = 500;
+    //       throw error;
+    //     }
+    //     console.log('s3.upload', data);
+    //     fileLocationArray.push(data.Location);
+    //   });
+    // });
   }
 }
 
