660e33969c9b508cbf33996a2896d1ae5d95836c
MinJunSeo <smj0385@naver.com>
2020-10-09 10:35:10 +0900

    Fix fixed is always true error

--- src/routes/noticeRouter.js
+++ src/routes/noticeRouter.js
@@ -6,33 +6,18 @@ const Notices = require('../models/notice');
 const NoticeService = require('../services/notice');
 const noticeService = new NoticeService(Notices);
 
-const { BUCKET_NAME } = require('../config');
-const s3 = new AWS.S3();
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: BUCKET_NAME,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: (req, file, cb) => {
-      const extension = path.extname(file.originalname);
-      cb(null, Date.now().toString() + extension);
-    },
-    acl: 'public-read'
-  })
-});
-
-router.post('/', upload.array('test'), async (req, res) => {
-  if (!req.body) {
+router.post('/',  async (req, res) => {
+  if (!Object.keys(req.fields).length) {
     return res.status(badRequest.status).send({
       message: badRequest.message
     });
   }
-  
+
   try {
-    const files = noticeService.uploadFiles(req.files);
-    const { title, content, fixed } = req.body;
-    
+    const { title, content } = req.fields;
+    const fixed = (req.fields.fixed !== 'false');
+    const files = await noticeService.uploadFiles(req.files.files);
+
     await noticeService.createNotice(title, content, files, fixed);    
     res.status(201).send({ message: '성공적으로 등록되었습니다.' });
   } catch (error) {
