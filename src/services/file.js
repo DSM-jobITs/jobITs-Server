2c16b34a87f8516ff4acc10ded584e6906d4ee40
MinJunSeo <smj0385@naver.com>
2020-10-09 10:25:15 +0900

    Fix return array error

--- src/services/file.js
+++ src/services/file.js
@@ -1,27 +1,11 @@
 const path = require('path');
 const makeUuid = require('../utils/makeUuid');
 const { badRequest } = require('../errors');
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
 
 const AWS = require('aws-sdk');
 AWS.config.loadFromPath(__dirname + '/../config/awsconfig.json');
-
 const { BUCKET_NAME } = require('../config');
 const s3 = new AWS.S3();
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: BUCKET_NAME,
-//     contentType: multerS3.AUTO_CONTENT_TYPE,
-//     key: (req, file, cb) => {
-//       const extension = path.extname(file.originalname);
-//       cb(null, Date.now().toString() + extension);
-//     },
-//     acl: 'public-read'
-//   })
-// });
 
 class FileService {
   makeFileKey(file) {
@@ -41,25 +25,18 @@ class FileService {
 
     let fileLocationArray = [];
     
-    
+    for (const file of files) {
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: this.makeFileKey(file),
+        Body: file.path,
+        ACL: 'public-read'
+      }
+      const data = await s3.upload(params).promise();
+      fileLocationArray.push(data.Location);
+    }
 
-    // files.forEach((file) => {
-    //   const params = {
-    //     Bucket: BUCKET_NAME,
-    //     Key: this.makeFileKey(file),
-    //     Body: file.path,
-    //     ACL: 'public-read'
-    //   };
-
-    //   s3.upload(params, (error, data) => {
-    //     if (error) {
-    //       error.status = 500;
-    //       throw error;
-    //     }
-    //     console.log('s3.upload', data);
-    //     fileLocationArray.push(data.Location);
-    //   });
-    // });
+    return fileLocationArray;
   }
 }
 
