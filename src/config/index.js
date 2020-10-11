554520b691ba627bb368fbeb4bcfab0277cd27d7
MinJunSeo <smj0385@naver.com>
2020-10-11 19:18:09 +0900

    Add bucket basic url

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,6 @@ module.exports = {
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
-  BUCKET_NAME: process.env.BUCKET_NAME
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  BUCKET_URL: process.env.BUCKET_URL
 };
