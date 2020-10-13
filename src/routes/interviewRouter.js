0bdb140b89df33532abaf1eac0a310209b3b48de
MinJunSeo <smj0385@naver.com>
2020-10-14 07:38:35 +0900

    Add check req.query arg error

--- src/routes/interviewRouter.js
+++ src/routes/interviewRouter.js
@@ -6,14 +6,21 @@ const interview = new Interview(Interviews);
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const { page } = req.query;
-  // field 값이 없다면 모든 field를 가져올 수 있게 하기 위해 false를 대입
-  // mysql의 모든 문자열은 boolean으로 바꾸면 false이다.
-  const field = req.query.field ? (req.query.field).split(',') : false;
-  const maxShow = 10;  // 현재 10개로 고정해두기로 결정
+const { badRequest } = require('../errors');
+const isNotObjctArgumnets = require('../utils/checkObjectArg');
 
+router.get('/', async (req, res) => {
   try {
+    if(isNotObjctArgumnets(req.query)) {
+      throw badRequest;
+    }
+    
+    const { page } = req.query;
+    // field 값이 없다면 모든 field를 가져올 수 있게 하기 위해 false를 대입
+    // mysql의 모든 문자열은 boolean으로 바꾸면 false이다.
+    const field = req.query.field ? (req.query.field).split(',') : false;
+    const maxShow = 10;  // 현재 10개로 고정해두기로 결정
+  
     const results = await interview.getInterviewQuestions(page, field, maxShow);
     res.send({ lists: results });
   } catch (error) {
