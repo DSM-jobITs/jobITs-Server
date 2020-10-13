feb4560adc1996733b5f9d78a7118527b1879d93
MinJunSeo <smj0385@naver.com>
2020-10-13 21:40:48 +0900

    Add models relation define model

---
+++ src/models/index.js
@@ -0,0 +1,21 @@
+const Notices = require('./notice');
+const FileMappings = require('./fileMappings');
+
+Notices.hasMany(FileMappings, {
+  foreignKey: 'noticeId',
+  sourceKey: 'id',
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+FileMappings.belongsTo(Notices, {
+  foreignKey: 'noticeId',
+  targetKey: 'id',
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  Notices,
+  FileMappings
+};
