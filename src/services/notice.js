025b471d604cb819d28fc52bc3172890c3c000eb
MinJunSeo <smj0385@naver.com>
2020-10-13 21:54:59 +0900

    Fix super arguments because fixing fileservice class

--- src/services/notice.js
+++ src/services/notice.js
@@ -4,76 +4,76 @@ const { BUCKET_URL } = require('../config');
 require('date-utils');
 
 class NoticeService extends FileService {
-  constructor(noticeModel) {
-    super();
+  constructor(noticeModel, fileMappingModel) {
+    super(fileMappingModel);
     this.noticeModel = noticeModel;
   }
 
-  async createNotice(title, content, fileNames, fixed) {
-    if ((typeof title !== 'string') || (typeof content !== 'string') || (typeof fixed !== 'boolean')) {
-      throw badRequest;
-    }
+  // async createNotice(title, content, fileNames, fixed) {
+  //   if ((typeof title !== 'string') || (typeof content !== 'string') || (typeof fixed !== 'boolean')) {
+  //     throw badRequest;
+  //   }
 
-    const result = await this.noticeModel.create({
-      title: title,
-      content: content,
-      file: fileNames,
-      fixed: fixed
-    });
+  //   const result = await this.noticeModel.create({
+  //     title: title,
+  //     content: content,
+  //     file: fileNames,
+  //     fixed: fixed
+  //   });
 
-    return result['id'];
-  }
+  //   return result['id'];
+  // }
 
-  async getOneNotice(id) {
-    const notice = await this.noticeModel.findOne({
-      attributes: ['title', 'content', 'file', 'createdAt'],
-      where: { id: id }
-    });
+  // async getOneNotice(id) {
+  //   const notice = await this.noticeModel.findOne({
+  //     attributes: ['title', 'content', 'file', 'createdAt'],
+  //     where: { id: id }
+  //   });
 
-    if (!notice) {
-      throw notFound;
-    }
+  //   if (!notice) {
+  //     throw notFound;
+  //   }
 
-    notice.createdAt = notice.createdAt.toFormat('YYYY-MM-DD');
-    if (notice.file) {
-      notice.file = notice.file.split('-');
-      for(let i = 0; i < notice.file.length; i++) {
-        notice.file[i] = BUCKET_URL + notice.file[i];
-      }
-    }
-    return notice;
-  }
+  //   notice.createdAt = notice.createdAt.toFormat('YYYY-MM-DD');
+  //   if (notice.file) {
+  //     notice.file = notice.file.split('-');
+  //     for(let i = 0; i < notice.file.length; i++) {
+  //       notice.file[i] = BUCKET_URL + notice.file[i];
+  //     }
+  //   }
+  //   return notice;
+  // }
 
-  async getNotices(page) {
-    if (page < 1) {
-      throw badRequest;
-    }
+  // async getNotices(page) {
+  //   if (page < 1) {
+  //     throw badRequest;
+  //   }
 
-    // 페이지네이션 10개로 고정해 둔 상황
-    const lists =  await this.noticeModel.findAll({
-      attributes: ['title', 'createdAt'],
-      offset: (page - 1) * 10,
-      limit: 10
-    });
+  //   // 페이지네이션 10개로 고정해 둔 상황
+  //   const lists =  await this.noticeModel.findAll({
+  //     attributes: ['title', 'createdAt'],
+  //     offset: (page - 1) * 10,
+  //     limit: 10
+  //   });
 
-    if (!lists.length) {
-      throw notFound;
-    }
-    for (let i = 0; i < lists.length; i++) {
-      lists[i].createdAt = lists[i].createdAt.toFormat('YYYY-MM-DD');
-    }
-    return lists;
-  }
+  //   if (!lists.length) {
+  //     throw notFound;
+  //   }
+  //   for (let i = 0; i < lists.length; i++) {
+  //     lists[i].createdAt = lists[i].createdAt.toFormat('YYYY-MM-DD');
+  //   }
+  //   return lists;
+  // }
 
-  async deleteNotice(id) {
-    const notice = await this.getOneNotice(id);
-    if (notice.file) {
-      await this.deleteFiles(notice.file);
-    }
-    await this.noticeModel.destroy({
-      where: { id: id }
-    });
-  }
+  // async deleteNotice(id) {
+  //   const notice = await this.getOneNotice(id);
+  //   if (notice.file) {
+  //     await this.deleteFiles(notice.file);
+  //   }
+  //   await this.noticeModel.destroy({
+  //     where: { id: id }
+  //   });
+  // }
 }
 
 module.exports = NoticeService;
