58ad519c9dc9e9e3a97154bc1e680812ce35b74e
MinJunSeo <smj0385@naver.com>
2020-10-13 22:50:57 +0900

    pull request test

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const { SERVER_PORT } = require('./config');
 const interviewRouter = require('./routes/interviewRouter');
 
 connectDatabase();
-
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
