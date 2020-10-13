291c7a4121b130cb9ca7e816d39f9ac0f3295cd6
MinJunSeo <smj0385@naver.com>
2020-10-13 22:09:25 +0900

    Use cors module

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectDatabase } = require('./models/connection');
+const cors = require('cors');
 const app = express();
 const { SERVER_PORT } = require('./config');
 const interviewRouter = require('./routes/interviewRouter');
@@ -8,6 +9,7 @@ connectDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/interview', interviewRouter);
 
