054c5e52c54f50db7c1937cba00f5bac731f7256
MinJunSeo <smj0385@naver.com>
2020-10-14 07:39:54 +0900

    Fix arguments init code

--- src/routes/interviewRouter.js
+++ src/routes/interviewRouter.js
@@ -14,52 +14,56 @@ router.get('/', async (req, res) => {
     if(isNotObjctArgumnets(req.query)) {
       throw badRequest;
     }
-    
-    const { page } = req.query;
+
+    const page = parseInt(req.query.page);
     // field 값이 없다면 모든 field를 가져올 수 있게 하기 위해 false를 대입
     // mysql의 모든 문자열은 boolean으로 바꾸면 false이다.
-    const field = req.query.field ? (req.query.field).split(',') : false;
-    const maxShow = 10;  // 현재 10개로 고정해두기로 결정
+    const field = req.query.field ? req.query.field : false;
+    const maxShow = 6;
   
     const results = await interview.getInterviewQuestions(page, field, maxShow);
-    res.send({ lists: results });
+    res.send({
+      lists: results
+    });
   } catch (error) {
-    res.status(error.status).send({ message: error.message });
+    res.status(error.status).send({
+      message: error.message
+    });
   }
 });
 
-router.post('/', async (req, res) => {
-  const { contents, field } = req.body;
+// router.post('/', async (req, res) => {
+//   const { contents, field } = req.body;
 
-  try {
-    await interview.registerInterviewQuestions(contents, field);
-    res.status(201).send({ message: '성공적으로 면접 질문을 등록하였습니다.' });
-  } catch (error) {
-    res.status(error.status).send({ message: error.message });
-  }
-});
+//   try {
+//     await interview.registerInterviewQuestions(contents, field);
+//     res.status(201).send({ message: '성공적으로 면접 질문을 등록하였습니다.' });
+//   } catch (error) {
+//     res.status(error.status).send({ message: error.message });
+//   }
+// });
 
-router.put('/:id', async (req, res) => {
-  const id = req.params.id;
-  const { content, field } = req.body;
+// router.put('/:id', async (req, res) => {
+//   const id = req.params.id;
+//   const { content, field } = req.body;
 
-  try {
-    await interview.modifyInterviewQuestion(id, content, field);
-    res.send({ message: '수정되었습니다.' });
-  } catch (error) {
-    res.status(error.status).send({ message: error.message });
-  }
-});
+//   try {
+//     await interview.modifyInterviewQuestion(id, content, field);
+//     res.send({ message: '수정되었습니다.' });
+//   } catch (error) {
+//     res.status(error.status).send({ message: error.message });
+//   }
+// });
 
-router.delete('/:id', async (req, res) => {
-  const id = req.params.id;
+// router.delete('/:id', async (req, res) => {
+//   const id = req.params.id;
 
-  try {
-    await interview.removeInterviewQuestion(id);
-    res.send({ message: '정상적으로 삭제되었습니다.' });
-  } catch (error) {
-    res.status(error.status).send({ message: error.message });
-  }
-});
+//   try {
+//     await interview.removeInterviewQuestion(id);
+//     res.send({ message: '정상적으로 삭제되었습니다.' });
+//   } catch (error) {
+//     res.status(error.status).send({ message: error.message });
+//   }
+// });
 
 module.exports = router;
