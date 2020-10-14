80cfb40b72eef6bd40555daeada88823c75b3ce5
MinJunSeo <smj0385@naver.com>
2020-10-14 17:07:19 +0900

    Fix /notice post routing

--- src/routes/noticeRouter.js
+++ src/routes/noticeRouter.js
@@ -6,78 +6,80 @@ const { Notices, FileMappings } = require('../models');
 const NoticeService = require('../services/notice');
 const noticeService = new NoticeService(Notices, FileMappings);
 
-router.get('/', async (req, res) => {
-  try {
-    const page = parseInt(req.query.page);
-    if (isNaN(page)) {
-      throw badRequest;
-    }
+// router.get('/', async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page);
+//     if (isNaN(page)) {
+//       throw badRequest;
+//     }
 
-    const lists = await noticeService.getNotices(page);
-    res.send({
-      lists: lists
-    });
-  } catch (error) {
-    res.status(error.status).send({
-      message: error.message
-    });
-  }
-});
+//     const lists = await noticeService.getNotices(page);
+//     res.send({
+//       lists: lists
+//     });
+//   } catch (error) {
+//     res.status(error.status).send({
+//       message: error.message
+//     });
+//   }
+// });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    if (isNaN(id)) {
-      throw badRequest;
-    }
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const id = parseInt(req.params.id);
+//     if (isNaN(id)) {
+//       throw badRequest;
+//     }
 
-    const result = await noticeService.getOneNotice(id);
-    res.send(result);
-  } catch (error) {
-    res.status(error.status).send({
-      message: error.message
-    });
-  }
-});
+//     const result = await noticeService.getOneNotice(id);
+//     res.send(result);
+//   } catch (error) {
+//     res.status(error.status).send({
+//       message: error.message
+//     });
+//   }
+// });
 
 router.post('/',  async (req, res) => {
   try {
-    if (req.fields.constructor !== Object || !Object.keys(req.fields).length) {
+    if (!Object.keys(req.fields).length) {
       throw badRequest;
     }
-
     const { title, content } = req.fields;
+    
     if (req.fields.fixed !== 'false' && req.fields.fixed !== 'true') {
       throw badRequest;
     }
     const fixed = (req.fields.fixed !== 'false');
-    const fileNames = await noticeService.integrateFileNames(req.files.files);
 
-    const noticeId = await noticeService.createNotice(title, content, fileNames, fixed);    
+    const noticeId = await noticeService.createNotice(title, content, fixed);
+    await noticeService.uploadFiles(noticeId, req.files.files);
+
     res.status(201).send({
       id: noticeId
     });
   } catch (error) {
-    res.status(error.status).send({
-      message: error.message
-    });
+    console.error(error);
+    // res.status(error.status).send({
+    //   message: error.message
+    // });
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    if (isNaN(id)) {
-      throw badRequest;
-    }
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const id = parseInt(req.params.id);
+//     if (isNaN(id)) {
+//       throw badRequest;
+//     }
 
-    await noticeService.deleteNotice(id);
-    res.send();
-  } catch (error) {
-    res.status(error.status).send({
-      message: error.message
-    });
-  }
-})
+//     await noticeService.deleteNotice(id);
+//     res.send();
+//   } catch (error) {
+//     res.status(error.status).send({
+//       message: error.message
+//     });
+//   }
+// });
 
 module.exports = router;
