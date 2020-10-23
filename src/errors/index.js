2e3f93a04473bd48f61b15631d8b01d2d8c0fc48
MinJunSeo <smj0385@naver.com>
2020-10-23 19:59:36 +0900

    Refactor error message

--- src/errors/index.js
+++ src/errors/index.js
@@ -5,8 +5,8 @@ class HttpError extends Error {
   }
 }
 
-const badRequest = new HttpError(400, '올바르지 않은 요청입니다.');
-const notFound = new HttpError(404, '게시물이 존재하지 않습니다.');
+const badRequest = new HttpError(400, 'Invalid parameters supplied');
+const notFound = new HttpError(404, 'Not found');
 
 module.exports = {
   badRequest,
