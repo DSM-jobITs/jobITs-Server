633b2fc6f888aa5241997bdda58c570e2e0ca46e
MinJunSeo <smj0385@naver.com>
2020-10-28 17:09:27 +0900

    feat: /company routing

---
+++ src/routes/company/index.js
@@ -0,0 +1,4 @@
+const router = require('express')();
+const controller = require('./controller');
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ const login = require('./login');
 const ping = require('./ping');
 const notice = require('./notice');
 const interview = require('./interview');
+const company = require('./company');
 
 router.use('/employment', employment);
 router.use('/login', login);
 router.use('/ping', ping); //test
 router.use('/notice', notice);
 router.use('/interview', interview);
+router.use('/company', company);
 
 module.exports = router;
