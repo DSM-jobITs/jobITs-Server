089bde36da36dc1d2e8f02e46b4322035e5927d3
MinJunSeo <smj0385@naver.com>
2020-10-28 18:58:31 +0900

    feat: getCompany service method

--- src/services/company.js
+++ src/services/company.js
@@ -1,9 +1,30 @@
 const { badRequest, notFound } = require('../errors');
+const EmployeRecordService = require('./employeRecord');
 
 class CompanyService {
   constructor(companyModel) {
     this.companyModel = companyModel;
   }
 
-  
+  async getCompany(companyId) {
+    if (typeof companyId !== 'number' || companyId < 1) {
+      throw badRequest;
+    }
+
+    const company = await this.companyModel.findOne({
+      attributes: ['name', 'region', 'introduction', 'logo'],
+      where: { id: companyId }
+    });
+
+    if (!company) {
+      throw notFound;
+    }
+
+    company.dataValues.totalEmployed =
+      await EmployeRecordService.getTotalEmployed(companyId);
+    company.dataValues.employedPeople = 
+      await EmployeRecordService.getEmployePeopleList(companyId);
+    
+    return company;
+  }
 }
