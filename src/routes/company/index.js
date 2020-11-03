98ed7f0e21784d39de2d686bef6c98ec6d6fa846
MinJunSeo <smj0385@naver.com>
2020-11-04 08:29:19 +0900

    fix: check jwt auth verify

--- src/routes/company/index.js
+++ src/routes/company/index.js
@@ -1,6 +1,7 @@
 const router = require('express')();
 const controller = require('./controller');
+const { authVerify } = require('../../middlewares/auth');
 
-router.get('/:id', controller.getCompany);
+router.get('/:id', authVerify, controller.getCompany);
 
 module.exports = router;
