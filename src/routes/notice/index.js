67fe67208b2753a13dc11d8187f78d87508c6e8e
MinJunSeo <smj0385@naver.com>
2020-11-07 13:20:15 +0900

    fix: notice get routing do not use auth middleware

--- src/routes/notice/index.js
+++ src/routes/notice/index.js
@@ -2,8 +2,8 @@ const router = require('express')();
 const controller = require('./controller');
 const { authVerify, isAdmin } = require('../../middlewares/auth');
 
-router.get('/', authVerify, controller.getNoticeList);
-router.get('/:id', authVerify, controller.getNotice);
+router.get('/', controller.getNoticeList);
+router.get('/:id', controller.getNotice);
 router.post('/', authVerify, isAdmin, controller.registerNotice);
 router.put('/:id', authVerify, isAdmin, controller.updateNotice);
 router.delete('/:id', authVerify, isAdmin, controller.removeNotice);
