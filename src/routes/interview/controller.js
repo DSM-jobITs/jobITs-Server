3f54e382a2688a5324cc08ffb994160f589a062a
MinJunSeo <smj0385@naver.com>
2020-11-11 16:11:13 +0900

    fix: numOfQuestion error

--- src/routes/interview/controller.js
+++ src/routes/interview/controller.js
@@ -23,11 +23,13 @@ const getInterviewList = async (req, res) => {
     const maxShow = 6;
 
     const results = await interview.getInterviewQuestions(page, field, keyword, maxShow);
+    const numOfQuestion = keyword ? results.length : await interview.getInterviewQuestions(field);
+    
     res.send({
       isAdmin: req.isAdmin,
       lists: results,
       field: field ? field : undefined,
-      numOfQuestion: results.length
+      numOfQuestion 
     });
   } catch (error) {
     res.status(error.status).send({
