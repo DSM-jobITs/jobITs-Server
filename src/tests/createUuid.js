d9eee1be100860ef48495cb56968600193901b9a
MinJunSeo <smj0385@naver.com>
2020-11-12 14:32:52 +0900

    style: remove console.log code

--- src/tests/createUuid.js
+++ src/tests/createUuid.js
@@ -1,3 +1,6 @@
 const makeUuid = require('../utils/makeUuid');
 
-console.log(makeUuid('마이다스아이티'));
+// console.log(makeUuid('(주)하얀마인드'));
+
+const checkExp = /^\s*$/;
+console.log(checkExp.test(' a d'));
