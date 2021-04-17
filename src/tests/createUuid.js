348b4d2392593398cc9d3e743bc87d383f5d902d
MinJunSeo <smj0385@naver.com>
2021-04-18 08:08:43 +0900

    docs: README.md

--- src/tests/createUuid.js
+++ src/tests/createUuid.js
@@ -1,6 +1,10 @@
 const makeUuid = require('../utils/makeUuid');
+require('date-utils');
 
 // console.log(makeUuid('(주)하얀마인드'));
 
-const checkExp = /^\s*$/;
-console.log(checkExp.test(' a d'));
+// const checkExp = /^\s*$/;
+// console.log(checkExp.test(' a d'));
+
+const date = new Date().toFormat('YYYY-MM-DD');
+console.log(date);
